1d6a843355e6aa6bb4a371fe74dda6fca97b1592
santi <santiagodev77@gmail.com>
2025-09-23 20:10:01 +0200

    Repaso Operadores!

---
+++ src/main/java/org/santiparra/com/flujosdecontrol/sentenciabucles/SentenciaForEach.java
@@ -0,0 +1,12 @@
+package org.santiparra.com.flujosdecontrol.sentenciabucles;
+
+public class SentenciaForEach {
+    public static void main(String[] args) {
+
+        int[] impares = {1, 3, 5, 7, 9, 11, 13, 14};
+
+        for (int num : impares) {
+            System.out.println("i = " + num);
+        }
+    }
+}
